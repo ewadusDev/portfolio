0acd7736d3ac273700c663ad6c012b6075e568cf
RUK KEOSUNG <rukzeedza@gmail.com>
2025-05-19 18:35:36 +0700

    refactor: imporve style for all devices in Header Component

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,12 @@ import { RxDownload } from "react-icons/rx";
 
 const Header = () => {
   return (
-    <header className="lg:h-[calc(100vh-64px)] ">
+    <header className=" lg:h-[calc(100vh-64px)] ">
       <div className="h-full flex flex-col lg:px-0 lg:flex-row ">
         {/* Left */}
-        <div className="flex flex-col justify-between gap-20 lg:gap-28 pt-5 pb-20 lg:pb-28 lg:basis-2/3 lg:p-12 px-2 sm:px-4 ">
-          <div className="flex flex-col gap-6">
-            <h1 className="font-extrabold text-7xl  sm:text-[196px] ">
+        <div className="flex flex-col justify-between gap-16 lg:gap-16 pt-5 pb-20 lg:pb-28 lg:basis-2/3 lg:p-12 px-2 sm:px-4">
+          <div className="flex flex-col gap-2  lg:gap-6 ">
+            <h1 className="font-extrabold text-7xl lg:text-[196px] ">
               Ruk
               <br /> Keosung
             </h1>
@@ -15,7 +15,7 @@ const Header = () => {
               QA FullStack Engineer{" "}
             </h2>
           </div>
-          <h3 className="text-xl lg:text-4xl font-normal">
+          <h3 className="text-xl lg:text-4xl ">
             Passionate about software testing, I enjoy building and creating
             interactive experiences.
           </h3>
@@ -30,7 +30,7 @@ const Header = () => {
           </div>
         </div>
         {/* Right */}
-        <div className="w-full h-[calc(100vh/2)] md:h-[calc(100vh)] lg:h-[calc(100vh-64px)] lg:basis-1/3 bg-gray-300"></div>
+        <div className="w-full h-[calc(100vh/2)] md:h-[calc(100vh)] lg:h-[calc(100vh-64px)] lg:basis-1/3 bg-gray-300" />
       </div>
     </header>
   );
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -44,30 +44,31 @@ const skills = [
 const Skill = () => {
   return (
     <section className="bg-black text-white">
-      <div className="flex flex-col">
+      <div className="flex flex-col lg:flex-row">
         {/* Left */}
-        <div className="border-b border-gray-300">
-          <h2 className="text-7xl px-6 py-8 font-normal">
+        <div className="border-b border-gray-300 lg:basis-2/5 lg:h-96 lg:border-r">
+          <h2 className="text-7xl px-6 py-8 lg:text-9xl h-full lg:p-14">
             My <br /> Skills
           </h2>
         </div>
         {/* Right */}
-        <div>
+        <div className="lg:basis-4/5">
+          <div className="hidden lg:block w-full h-96 border-b border-gray-300" />
           {skills.map((skill, index) => {
             return (
               <div className="flex justify-between" key={index}>
-                <div className="bg-gray-300 basis-1/4 w-full">
+                <div className="basis-1/4 w-full bg-gray-300 ">
                   {/* Image */}
                 </div>
-                <div className="flex basis-3/4 py-10  sm:py-16 border-b border-gray-200 ">
-                  <div className="basis-1/3 ">
-                    <p className="rounded-4xl border border-[#0AB274]  w-fit mx-auto py-1 px-4">
+                <div className="flex basis-3/4 py-10 sm:py-16 border-b border-gray-200 ">
+                  <div className="basis-1/3 lg:basis-1/6 lg:py-8">
+                    <p className="rounded-4xl border border-[#0AB274] w-fit mx-auto py-1 px-4 lg:text-2xl ">
                       0{index + 1}
                     </p>
                   </div>
-                  <div className="basis-2/3">
-                    <h6 className="text-3xl">{skill.name}</h6>
-                    <p className="mt-4">{skill.description}</p>
+                  <div className="basis-2/3 lg:basis-5/6 lg:py-8">
+                    <h6 className="text-3xl lg:text-6xl">{skill.name}</h6>
+                    <p className="mt-4 lg:text-xl">{skill.description}</p>
                   </div>
                 </div>
               </div>
